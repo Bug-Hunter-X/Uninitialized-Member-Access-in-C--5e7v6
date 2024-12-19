00a6e84be5a6f04fe1227390cf0ca176087b15d0
BugHunter-X <askzenai@gmail.com>
2024-12-19 16:36:05 +0100

    Add Bug.cs file

---
+++ Bug.cs
@@ -0,0 +1,15 @@
+public class ExampleClass
+{
+    public int MyProperty { get; set; }
+
+    public ExampleClass(int value)
+    {
+        MyProperty = value;
+    }
+
+    public void MyMethod()
+    {
+        // Accessing a member that is not initialized properly
+        Console.WriteLine(MyProperty);
+    }
+}
